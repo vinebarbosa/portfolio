c8c5ac28cbcb89ed0934266a54cf412fc0333380
Vinícios Barbosa <viniciosbarbosa740@gmail.com>
2024-05-25 13:43:00 -0300

    refactor: :iphone: melhora layout mobile

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export const Logo = () => {
   return (
     <Link href="/">
-      <div className="font-bold text-3xl text-gray-900 dark:text-white">
+      <div className="font-bold text-2xl md:text-3xl text-gray-900 dark:text-white">
         Vinícios Barbosa
       </div>
     </Link>
